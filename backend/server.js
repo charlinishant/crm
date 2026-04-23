74e260b322014748f3954ab3e88836e5f2d3a0cc
nilesh <64306984+nileshkharat-git@users.noreply.github.com>
2026-04-23 21:35:33 +0530

    [FEAT]  Lead model and  CRUD api's

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+
+const app = express();
+
+const leadRouter = require('./routes/leadRoutes')
+
+app.use(cors());
+app.use(express.json());
+app.use('/leads', leadRouter)
+
+app.get('/test', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT 1');
+    res.send('Database Connected ');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
